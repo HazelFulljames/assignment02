45d9fea1015f7b06f3af16d81ecf40380cffe0b6
Hazel <fulljameshazel@gmail.com>
2025-05-18 00:30:23 -0700

    1.01

--- public/route.js
+++ public/route.js
@@ -10,11 +10,17 @@ const router = Router();
 router.post('/addPearl', async (req, res) => {
   const { x, z, type, townx, townz } = req.body;
   console.log(req.body)
-  if (x == null) {
+  if (x == null || x == NaN) {
     console.log("data invalid", req.body);
     return res.status(500).json({error: "a"});
   } else {
-    await pearlCollection.insertOne({town: { x: townx, z: townz }, type: type, x: x, z: z, date: new Date()});
+    await pearlCollection.insertOne({
+        town: { x: townx, z: townz }, 
+        type: type, 
+        x: x, z: z, 
+        date: new Date(),
+        addedBy: req.session.username
+    });
     return res.status(200).json({success: "a"});
   }
 });
